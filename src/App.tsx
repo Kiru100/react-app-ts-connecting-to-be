c9b0d2db75324ce016c6a478125c2a86d4981d79
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-26 20:02:15 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect, useRef, useState } from 'react';
-import ProductList from './components/ProductList';
+import { useEffect } from 'react';
+
+const connect = () => console.log("connecting");
+const disconnect = () => console.log("disconnecting");
+
 
 function App() {
-	const input_ref = useRef<HTMLInputElement>(null);
-	const [category, setCategory] = useState("");
-
 	useEffect(()=>{
-		if(input_ref.current) input_ref.current.focus();
-	},[])
-
-	useEffect(()=>{
-		document.title = "Ha";
-	},[])
-	
+		connect();
+		
+		return ()=> disconnect();
+	});
 
 	return (
 		<div>
-			<select className='form-select' onChange={(event)=>setCategory(event.target.value)}>
-				<option value=""></option>
-				<option value="Clothing">Clothing</option>
-				<option value="Household">Household</option>
-			</select>
-			<ProductList category={category}/>
+			
 		</div>
 	)
 }
