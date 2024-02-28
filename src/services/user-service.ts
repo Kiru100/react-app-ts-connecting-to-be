77c3e5e95e4966aa875eb46a51ec63263231e966
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-28 09:32:43 +0800

    Update user-service.ts

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,28 +1,9 @@
 import apiClient from "./api-client";
+import create from "./http-service";
 
 export interface User{
 	id:number,
 	name: string
 }
 
-class UserService{
-    getAllUsers(){
-        const controller = new AbortController();
-        const request =  apiClient.get<User[]>("/users", {signal: controller.signal});
-        return { request, cancel: ()=> controller.abort()}
-    }
-
-    deleteUser(id: number){
-        return apiClient.delete("/users/" + id);
-    }
-
-    addUser(new_user: object){
-        return apiClient.post("/users/", new_user);
-    }
-
-    updateUser(updated_user: User){
-        return apiClient.patch("/users/" + updated_user.id, updated_user)
-    }
-}
-
-export default new UserService();
+export default create("/users");
