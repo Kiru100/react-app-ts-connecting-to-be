80ef44853f6f1ed5974ef86124bc8aaa33b43c2c
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-02-28 09:42:51 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,10 @@ import { useEffect, useState } from 'react';
 
 import apiClient, {CanceledError} from './services/api-client';
 import userService, { User } from './services/user-service';
+import useUsers from './hooks/userUsers/useUsers';
 
 function App() {
-	const [users, setUsers] = useState<User[]>([]);
-	const [error, setError] = useState("");
-	const [isLoading, setLoading] = useState(false);
-	
-	useEffect(()=> {
-		/* Get -> promise -> res / err */
-		setLoading(true);
-		const {request, cancel}  = userService.getAll<User>();
-
-		request
-			.then(res=>{
-				setUsers(res.data)
-				setLoading(false);
-			})
-			.catch(err =>{setError(err.message)
-				if(err instanceof CanceledError) return;
-				setError(err.message);
-				setLoading(false);
-			})
-
-		return () => cancel();
-	},[]);
+	const {users, error, isLoading, setUsers, setError} = useUsers(); 
 
 	const deleteUser = (user: User) =>{
 		const originalUsers = [...users];
