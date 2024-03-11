d97b95845efe3270f9967ea3ea66b96ff550a2f0
Noel Dasco Jr <79371033+Kiru100@users.noreply.github.com>
2024-03-11 21:34:03 +0800

    Update ProductList.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -7,11 +7,9 @@ export default function ProductList({category}: {category: string}) {
     const [products, setProducts] = useState<string[]>([]);
 
     useEffect(()=>{
-
         console.log("fetching products in", category);
-        setProducts(["clothing", "household"]);
-        
-    }, [category])
+        setProducts(["clothing", "household"]);  
+    }, [category]);
 
     return (
         <div>ProductList {products}</div>
